a47d16a699ed43617a8d65d7639cd3d572e39b58
Aman <arl0817osho@gmail.com>
2024-08-04 19:00:03 +0530

    Update SHC_BACKEND_API_BASE_URL constant to point to production environment

--- src/consts.rs
+++ src/consts.rs
@@ -1,4 +1,4 @@
-pub const SHC_BACKEND_API_BASE_URL: &str = "https://shc.up.railway.app";
+pub const SHC_BACKEND_API_BASE_URL: &str = "https://shc-backend-production.up.railway.app";
 
 pub const SHC_CLI_FOLDER_NAME: &str = ".shc-cli";
 pub const USER_CONFIG_FILE_NAME: &str = "config.toml";
